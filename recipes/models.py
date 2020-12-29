dc0536f28242ef04bfe26e3218e121dfde61cd08
Brian Oliver <semperindoctrina19@gmail.com>
2020-12-29 02:38:41 -0600

    recipe model finished and added to django admin

---
+++ recipes/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=150)
+    intro = models.TextField()
+    prep_time = models.DurationField()
+    cook_time = models.DurationField()
+    servings = models.IntegerField()
+
