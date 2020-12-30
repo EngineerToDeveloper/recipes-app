63bded6fc879cefda573779cab7c648eb8e6fde5
Brian Oliver <semperindoctrina19@gmail.com>
2020-12-30 00:53:33 -0600

    part 1 completed code

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RecipeForm
 
-# Create your views here.
+def create_recipe(request):
+    if request.method == "GET":
+        form = RecipeForm()
+        return render(request, 'create_recipe.html', {"form":form})
+    elif request.method == "POST":
+        form = RecipeForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+        else:
+            return render(request, 'create_recipe.html', {"form":form})
