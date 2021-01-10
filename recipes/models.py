1be7ac9118b9a1146d2bc13a27e750e044a510b4
Brian Oliver <semperindoctrina19@gmail.com>
2021-01-09 18:37:20 -0600

    updated template and view

--- recipes/models.py
+++ recipes/models.py
@@ -7,3 +7,11 @@ class Recipe(models.Model):
     cook_time = models.DurationField()
     servings = models.IntegerField()
 
+    def __str__(self):
+        return self.title
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+    quantity = models.FloatField()
+
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name="ingredients", null=True)
